eab9e7057b57ebd654e2f46175550963c6401d12
Yuto Watanabe <yuto.w51942@gmail.com>
2021-04-06 11:16:29 +0900

    fix create readme script

--- create_readme.sh
+++ create_readme.sh
@@ -3,7 +3,8 @@
 if [ $1 ] ; then
 date=`date +'%Y年%m月%d日（%A）'`
 cd section${1} && touch README.md \
-&& echo "# 第${1}回目 [タイトル]\n\n${date}" > README.md
+&& echo "# 第${1}回目 [タイトル]\n\n${date}" > README.md \
+&& echo "[教科書](http://edu.net.c.dendai.ac.jp/ad2/2021/${1})" >> README.md
 else
 echo "Please section number"
 echo "example:"
